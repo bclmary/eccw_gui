5744248c8d5ee2e25205ecd2927e5c285be89574
BCL Mary <baptiste.mary@u-cergy.fr>
2019-10-10 16:25:38 +0200

    update pip packaging

--- setup.py
+++ setup.py
@@ -4,51 +4,42 @@
 from setuptools import setup, find_packages
 import sys
 from distutils.command.install_data import install_data
-import subprocess
-import os.path as osp
 
+import subprocess
+import os
 
 def get_data_files():
     """Return data_files in a platform dependent manner"""
-    if sys.platform.startswith('linux'):
-        data_files = [('share/applications', ['scripts/eccw.desktop']),
-                      ('share/pixmaps', [
-                        'eccw_gui/images/icon_eccw.svg',
-                        #'eccw_gui/images/icon_eccw_16x16.png',
-                        #'eccw_gui/images/icon_eccw_24x24.png',
-                        #'eccw_gui/images/icon_eccw_32x32.png',
-                        #'eccw_gui/images/icon_eccw_48x48.png',
-                        #'eccw_gui/images/icon_eccw_128x128.png',
-                        #'eccw_gui/images/icon_eccw_256x256.png',
-                        #'eccw_gui/images/icon_eccw_512x512.png'
-                        ]),
-                      ('share/metainfo', ['scripts/eccw.appdata.xml'])]
+    if sys.platform.startswith("linux"):
+        data_files = [
+            ("share/applications", ["scripts/eccw.desktop"]),
+            ("share/pixmaps", ["eccw_gui/images/icon_eccw.svg"]),
+            ("share/icons", ["eccw_gui/images/icon_eccw.svg"]),
+            ("share/metainfo", ["scripts/eccw.appdata.xml"]),
+        ]
     else:
         data_files = []
     return data_files
-
 
 # Make Linux detect ECCW desktop file
 class MyInstallData(install_data):
     def run(self):
         install_data.run(self)
-        if sys.platform.startswith('linux'):
+        if sys.platform.startswith("linux"):
             try:
-                subprocess.call(['update-desktop-database'])
+                subprocess.call(["update-desktop-database"])
             except:
-                print("ERROR: unable to update desktop database",
-                      file=sys.stderr)
-CMDCLASS = {'install_data': MyInstallData}
+                print("ERROR: unable to update desktop database", file=sys.stderr)
 
+CMDCLASS = {"install_data": MyInstallData}
+
+SCRIPTS = ["eccw"]
+if not sys.platform.startswith('linux'):
+    SCRIPTS.append("eccw_win_post_install.py")
 
 setup(
-    entry_points={
-        'console_scripts': [
-            'eccw=eccw_gui.main:launch',
-            ],
-        },
+    entry_points={"console_scripts": ["eccw=eccw_gui.main:launch"]},
+    scripts=[os.path.join('scripts', fname) for fname in SCRIPTS],
     data_files=get_data_files(),
-    scripts=[osp.join('scripts', 'eccw')],
     cmdclass=CMDCLASS,
-    )
-
+)
