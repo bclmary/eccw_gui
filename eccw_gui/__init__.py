54bc5054a206a0382f6153ea6e73bd7a51ad214c
BCL Mary <baptiste.mary@u-cergy.fr>
2019-10-10 16:27:29 +0200

    all metadatas are now really centralised in setup.cfg

--- eccw_gui/__init__.py
+++ eccw_gui/__init__.py
@@ -37,40 +37,33 @@ Exact Critical Coulomb Wedge - Graphical User Interface.
 """
 
 import pkg_resources
-import os
-from setuptools.config import read_configuration
-
-
-def _extract_metadata(metadata_name):
-    """Get metadata from pip installation."""
-    return getattr(pkg_resources.get_distribution("eccw_gui"), metadata_name)
 
 try:
-    __version__ = _extract_metadata("version")
-except pkg_resources.DistributionNotFound:
-    _conf = read_configuration(os.path.join(
-        os.path.dirname(os.path.dirname(__file__)), 'setup.cfg')
-        )
-    __version__ = _conf['metadata']["version"]
+    eccw_distrib = pkg_resources.get_distribution("eccw_gui")
+    __location__ = eccw_distrib.location
+    eccw_provider = eccw_distrib._provider
+    eccw_metadata = eval(eccw_provider.get_metadata("metadata.json"))
+    __version__ = eccw_metadata["version"]
+    __license__ = eccw_metadata["license"]
+    details = eccw_metadata["extensions"]["python.details"]
+    __url__ = details["project_urls"]["Home"]
+    for elt in details["contacts"]:
+        if elt["role"] == "author":
+            __authors__ = [name.strip() for name in elt["name"].split(",")]
+        if elt["role"] == "maintainer":
+            __contact__ = elt["email"]
+except (pkg_resources.DistributionNotFound, FileNotFoundError):
+    import os
+    from setuptools.config import read_configuration
 
-try:
-    __location__ = _extract_metadata("location")
-except pkg_resources.DistributionNotFound:
-    for path in os.environ['PYTHONPATH'].split(os.pathsep):
-        if path.endswith('eccw_gui/') or path.endswith('eccw_gui'):
+    metadata = read_configuration(
+        os.path.join(os.path.dirname(os.path.dirname(__file__)), "setup.cfg")
+    )["metadata"]
+    __version__ = metadata["version"]
+    __license__ = metadata["license"]
+    __url__ = metadata["url"]
+    __contact__ = metadata["maintainer_email"]
+    __authors__ = [name.strip() for name in metadata["author"].split(",")]
+    for path in os.environ["PYTHONPATH"].split(os.pathsep):
+        if path.endswith("eccw_gui/") or path.endswith("eccw_gui"):
             __location__ = path
-
-__authors__ = [
-    'BCL Mary',
-    'Xiaoping Yuan',
-    'YM Leroy',
-    'Pauline Souloumiac',
-    'Chong Wu',
-    ]
-
-__license__ = "GNU GPL-v3"
-
-__url__ = "https://github.com/bclmary"
-
-__contact__ = "bclmary@mailoo.org"
-
